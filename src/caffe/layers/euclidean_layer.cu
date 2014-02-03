95d28f7a34fff63a7c44683f76a2b390f0764faf
Aravindh Mahendran <aravindh@aravindh-VirtualBox.(none)>
2014-02-02 21:16:44 -0500

    * Fixed bug to take top_diff into account in eucliean layer. Also not computing loss as its not a loss type layer. * Attempting to speed up caffe_gpu_sub by grid stride kernel design.

--- src/caffe/layers/euclidean_layer.cu
+++ src/caffe/layers/euclidean_layer.cu
@@ -17,7 +17,7 @@ template <typename Dtype>
 void EuclideanLayer<Dtype>::SetUp(
   const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
   CHECK_EQ(bottom.size(), 2) << "Loss Layer takes two blobs as input.";
-  CHECK_LE(top->size(), 1) << "Loss Layer takes atmost one output.";
+  CHECK_EQ(top->size(), 1) << "Loss Layer takes one output.";
   CHECK_EQ(bottom[0]->num(), bottom[1]->num())
       << "The data1 and data2 should have the same number.";
   CHECK_EQ(bottom[0]->channels(), bottom[1]->channels());
@@ -25,15 +25,12 @@ void EuclideanLayer<Dtype>::SetUp(
   CHECK_EQ(bottom[0]->width(), bottom[1]->width());
   difference_.Reshape(bottom[0]->num(), bottom[0]->channels(),
       bottom[0]->height(), bottom[0]->width());
-  if(top->size() == 1) {
-      (*top)[0]->Reshape(1, 1, 1, 1);
-  }
+  (*top)[0]->Reshape(1, 1, 1, 1);
 }
 
 template <typename Dtype>
 void EuclideanLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     vector<Blob<Dtype>*>* top) {
-  if(top->size() == 1) {
     int count = bottom[0]->count();
     int num = bottom[0]->num();
     caffe_sub(count, bottom[0]->cpu_data(), bottom[1]->cpu_data(),
@@ -41,13 +38,11 @@ void EuclideanLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     Dtype loss = caffe_cpu_dot(
         count, difference_.cpu_data(), difference_.cpu_data()) / num;
     (*top)[0]->mutable_cpu_data()[0] = loss;
-  }
 }
 
 template <typename Dtype>
 void EuclideanLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     vector<Blob<Dtype>*>* top) {
-  if(top->size() == 1) {
     int count = bottom[0]->count();
     int num = bottom[0]->num();
     caffe_gpu_sub(count, bottom[0]->gpu_data(), bottom[1]->gpu_data(),
@@ -56,39 +51,46 @@ void EuclideanLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     caffe_gpu_dot(count, 
         difference_.gpu_data(), difference_.gpu_data(), &loss);
     (*top)[0]->mutable_cpu_data()[0] = loss/num;
-  }
 }
 
 template <typename Dtype>
 Dtype EuclideanLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const bool propagate_down, vector<Blob<Dtype>*>* bottom) {
-  int count = (*bottom)[0]->count();
-  int num = (*bottom)[0]->num();
-  caffe_sub(count, (*bottom)[0]->cpu_data(), (*bottom)[1]->cpu_data(),
-      difference_.mutable_cpu_data());
-  Dtype loss = caffe_cpu_dot(
-      count, difference_.cpu_data(), difference_.cpu_data()) / num / Dtype(2);
-  // Compute the gradient
-  caffe_axpby(count, Dtype(1) / num, difference_.cpu_data(), Dtype(0),
-      (*bottom)[0]->mutable_cpu_diff());
-  return loss;
+  if(propagate_down) {
+    const Dtype top_diff = *(top[0]->cpu_diff());
+    int count = (*bottom)[0]->count();
+    int num = (*bottom)[0]->num();
+    caffe_sub(count, (*bottom)[0]->cpu_data(), (*bottom)[1]->cpu_data(),
+        difference_.mutable_cpu_data());
+    //This is not a layer layer type. Its computing the error and sending it further down the network. 
+    //Dtype loss = caffe_cpu_dot(
+    //    count, difference_.cpu_data(), difference_.cpu_data()) / num / Dtype(2);
+    // Compute the gradient
+    caffe_axpby(count, top_diff / num, difference_.cpu_data(), Dtype(0),
+        (*bottom)[0]->mutable_cpu_diff());
+  }
+  return Dtype(0);
 }
 
 template <typename Dtype>
 Dtype EuclideanLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     const bool propagate_down, vector<Blob<Dtype>*>* bottom) {
-  int count = (*bottom)[0]->count();
-  int num = (*bottom)[0]->num();
-  caffe_gpu_sub(count, (*bottom)[0]->gpu_data(), (*bottom)[1]->gpu_data(),
-      difference_.mutable_gpu_data());
-  Dtype loss;
-  caffe_gpu_dot(
-      count, difference_.gpu_data(), difference_.gpu_data(), &loss);
-  loss = loss / num / Dtype(2);
-  // Compute the gradient
-  caffe_gpu_axpby(count, Dtype(1) / num, difference_.gpu_data(), Dtype(0),
-      (*bottom)[0]->mutable_gpu_diff());
-  return loss;
+  if(propagate_down) {
+    const Dtype top_diff = *(top[0]->cpu_diff());
+    int count = (*bottom)[0]->count();
+    int num = (*bottom)[0]->num();
+    caffe_gpu_sub(count, (*bottom)[0]->gpu_data(), (*bottom)[1]->gpu_data(),
+        difference_.mutable_gpu_data());
+    //This is not a layer layer type. Its computing the error and sending it further down the network. 
+    //Dtype loss;
+    //caffe_gpu_dot(
+    //    count, difference_.gpu_data(), difference_.gpu_data(), &loss);
+    //loss = loss / num / Dtype(2);
+    // Compute the gradient
+    caffe_gpu_axpby(count, top_diff / num, difference_.gpu_data(), Dtype(0),
+        (*bottom)[0]->mutable_gpu_diff());
+  }
+  return Dtype(0);
 }
 
 
--- src/caffe/util/math_functions.cu
+++ src/caffe/util/math_functions.cu
@@ -35,23 +35,25 @@ void caffe_gpu_mul<double>(const int N, const double* a,
 template <typename Dtype>
 __global__ void sub_kernel(const int n, const Dtype* a,
     const Dtype* b, Dtype* y) {
-  int index = threadIdx.x + blockIdx.x * blockDim.x;
-  if (index < n) {
-    y[index] = a[index] - b[index];
+  for(int i = threadIdx.x + blockIdx.x * blockDim.x; 
+        i < n;
+        i += blockDim.x + gridDim.x)
+  {
+     y[i] = a[i] - b[i];
   }
 }
 
 template <>
 void caffe_gpu_sub<float>(const int N, const float* a,
     const float* b, float* y) {
-  sub_kernel<float><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
+  sub_kernel<float><<<CAFFE_GET_BLOCKS(N)/10, CAFFE_CUDA_NUM_THREADS>>>(
       N, a, b, y);
 }
 
 template <>
 void caffe_gpu_sub<double>(const int N, const double* a,
     const double* b, double* y) {
-  sub_kernel<double><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
+  sub_kernel<double><<<CAFFE_GET_BLOCKS(N)/10, CAFFE_CUDA_NUM_THREADS>>>(
       N, a, b, y);
 }
 
